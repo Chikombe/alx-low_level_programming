0a53b2a1bc8f206597ff18e30723bd9e41eda44a
Chikombe <chikombe.ecy@gmail.com>
2023-04-24 10:35:02 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints alpha in lower case
+ * folloewd by a new line, except q and e
+ * Return: Always 0
+ */
+int main(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; ch++)
+	{
+		if (la != 'e' && la != 'q')
+			putchar(la);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
