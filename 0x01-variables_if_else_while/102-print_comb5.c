98574369ef73f2487bc981c156b23d5dc425f585
Chikombe <chikombe.ecy@gmail.com>
2023-04-24 14:52:48 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	int i, j;
 
-	for (i = '0'; i <= '99'; i++)
+	for (i = 0; i <= 99; i++)
 	{
-		for (j = i + 1; j <= '99'; j++)
+		for (j = i + 1; j <= 99; j++)
 		{
 			putchar('0' + i / 10);
 			putchar('0' + i % 10);
@@ -18,7 +18,7 @@ int main(void)
 			putchar('0' + j / 10);
 			putchar('0' + j % 10);
 
-			if (i == '98' && j == '99'
+			if (i == 98 && j == 99
 					break;
 			putchar(',');
 			putchar(' ');
