fcd25134141e944dc61fe92b9d36c02d779c9f18
Chikombe <chikombe.ecy@gmail.com>
2023-04-26 14:10:53 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,19 +7,37 @@
  */
 void print_to_98(int n)
 {
-	if (n >= 98)
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+			i
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
+		}
+	}
+	else
 	{
 		for (; n >= 98; n--)
 		{
-			printf("%d, ", n--);
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
 		}
 	}
-	else if (n < 98)
-	{
-		for (; n < 98; n++)
-		{
-			printf("%d, ", n++);
-		}
-	}
-	printf("%d\n", n);
 }
