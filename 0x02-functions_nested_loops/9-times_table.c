8b7a40992ae1f71cb11f32db438d6c0d8867de55
Chikombe <chikombe.ecy@gmail.com>
2023-04-26 11:08:09 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,21 +9,22 @@ void times_table(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		_putchar('0');
+		_putchar(48);
 
 		for (mult = 1; mult <= 9; mult++)
 		{
-			_putchar(',');
-			_putchar(' ');
+			_putchar(44);
+			_putchar(32);
 
 			prod = num * mult;
 
 			if (prod <= 9)
-				_putchar(' ');
+				_putchar(32);
+				_putchar(prod + 48);
 			else
 			{
-				_putchar((prod / 10) + '0');
-				_putchar((prod % 10) + '0');
+				_putchar((prod / 10) + 48);
+				_putchar((prod % 10) + 48);
 			}
 		}
 		_putchar('\n');
