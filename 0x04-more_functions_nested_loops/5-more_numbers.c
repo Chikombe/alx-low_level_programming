7ca6a297addce5143215e40f529018ab324172ae
Chikombe <chikombe.ecy@gmail.com>
2023-05-01 13:35:21 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,9 +13,10 @@ void more_numbers(void)
 	{
 		for (num = '0'; num <= 14; num++)
 		{
-			_putchar(num);
-		}
-		_putchar('\n');
+			if (num >= 10)
+				_putchar('1');
+			_putchar (num % 10 + '0');
+		{
 	}
-	_putchar('\n');
+		_putchar('\n')
 }
