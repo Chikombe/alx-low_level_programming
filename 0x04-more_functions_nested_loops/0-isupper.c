ebaadf4a575c121232b005531fc85c6d6b0f8dc4
Chikombe <chikombe.ecy@gmail.com>
2023-05-01 08:06:13 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: the character
+ * Return: 1 on success 0 otherwise
+ */
+
+int _isupper(int c);
+{
+	char c;
+
+	if (c == "C")
+	{
+		return (1);
+	}
+	else if (c == "c")
+	{
+		return (0);
+	}
+}
