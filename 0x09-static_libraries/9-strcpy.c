053f83cf1ca3a879c9e79ee97353d11428873dcc
Chikombe <chikombe.ecy@gmail.com>
2023-05-15 00:35:17 -0700

    updated

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcpy - a function that copies the string pointed to bt src
+ * @dest: copy to
+ * @src: copy from
+ * Return: copied string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int l = 0;
+	int x = 0;
+
+	while (*(src + l) != '\0')
+	{
+		l++;
+	}
+	for (; x < l; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[l] = '\0';
+	return (dest);
+}
