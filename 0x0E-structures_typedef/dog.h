fccde32ac4331a83a125f05006dd9c8d925d46d0
Chikombe <chikombe.ecy@gmail.com>
2023-05-22 23:36:00 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -21,7 +21,7 @@ struct dog
  * dog_t - typedef for struct dog
  */
 
-typedef struct dog(dog_t);
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
@@ -31,3 +31,4 @@ char _strcpy(char *dest, char *src);
 int _strlen(char *s);
 
 #endif
+
