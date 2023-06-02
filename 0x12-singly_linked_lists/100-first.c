a0ba8ba95d4ddb6a564e3bd8fd1b9aa917970ac3
Chikombe <chikombe.ecy@gmail.com>
2023-06-02 13:31:22 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 
-void print_before_main(void);
+void first(void) __attribute__ ((constructor));
 
 /**
- * main - prints a sentence before the main fuction is executed
- * Return: 0
+ * first - prints a sentence before the main fuction is executed
  */
 
-int main(void)
+void first(void)
 {
 	printf("You're beat! and yet, you must allow\n");
 	printf("I bore my house upon my back!\n");
 
-	return (0);
 }
