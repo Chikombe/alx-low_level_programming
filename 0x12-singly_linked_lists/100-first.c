325b7466b35c0610bf41cafe3b8e97f022bf2e86
Chikombe <chikombe.ecy@gmail.com>
2023-06-02 13:21:15 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void print_before_main(void);
+
+/**
+ * main - prints a sentence before the main fuction is executed
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("You're beat! and yet, you must allow\n");
+	printf("I bore my house upon my back!\n");
+
+	return (0);
+}
