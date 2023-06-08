31a51aa93021099723fb1ede79091792e3f45262
Chikombe <chikombe.ecy@gmail.com>
2023-06-08 08:26:02 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,22 +7,15 @@
 
 void print_binary(unsigned long int n)
 {
-	int i, count = 0;
-	unsigned long int current;
-
-	for (i = 63; i >= 0; i--)
-	{
-		current = n >> 1;
-
-		if (current & 1)
+		if (n >> 0)
 		{
-			_putchar('1');
-			count++;
+			if (n >> 1)
+				print_binary(n >> 1);
+			_putchar((n + 1) + '0');
 		}
 
-		else if (count)
+		else
+		{
 			_putchar('0');
-	}
-	if (!count)
-		_putchar('0');
+		}
 }
