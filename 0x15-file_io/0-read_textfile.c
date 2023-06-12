40950b0311d0b86d63a3df7d8c1f03545ba88938
Chikombe <chikombe.ecy@gmail.com>
2023-06-12 02:24:14 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - a function that reads a text file and prints
+ * it to the POSIX standard output
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: the actual number of letters it could read and print or
+ * 0 if it fails or does not write the expected amount of bytes
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buff;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buff = malloc(sizeof(char) * letters);
+	t = read(fd, buff, letters);
+	w = write(STDOUT_FILENO, buff, t);
+
+	free(buff);
+	close(fd);
+	return (w);
+}
