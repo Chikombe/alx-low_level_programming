5699bf4c685abf273b895788b23ce70f13360286
chikombe <chikombe.ecy@gmail.com>
2023-08-22 01:36:10 -0700

    task 0 dynamic libraries

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: the character
+ * Return: 1 on success 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
